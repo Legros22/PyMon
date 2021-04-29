ad8a3fc97d81271152748e12152ccb28df97d1fd
philippe <phil_legros@yahoo.fr>
2021-04-29 21:19:16 +0200

    Fix button size, json save, add <Start Measure Rf button>

--- PyMon_ClientInWindow.py
+++ PyMon_ClientInWindow.py
@@ -17,7 +17,7 @@ import json
 
 root = Tk()
 root.title('PyMon CLient (ZIOT)')
-root.geometry('{}x{}'.format(800, 350))
+root.geometry('{}x{}'.format(800, 550))
 
 # set default color
 WIN_FORGROUNG = "#EEE"
@@ -30,105 +30,146 @@ LABEL_BACKGROUND = WIN_BACKGROUND #"cyan"
 LABEL_FOREGROUND = WIN_FORGROUNG
 ENTRY_COLOR     = CMD_BACKGROUND #"pink"
 
+BUTTON_DEF_WIDTH = 10
+ENTRY_DEF_WIDTH = 15
 
 ## Manage software config in json file
 ## ------------------------------------
 
-##ConfData0 = {
-##    "Ethernet": {
-##        "ADDRESS" : "192.168.1.52",
-##        "MASK"    : "255.255.0.0",
-##        "PORT"    : "6789"
-##        },
-##    "Log":{
-##        "LogFileName":"MonLog.txt",
-##        "LogDelimiter":";"
-##        }
-##    }
-##
-##MyJSON = json.dumps(ConfData0, indent=4)
-##
-##with open("MonZiot.json", "w") as jsonfile:
-##    jsonfile.write(MyJSON)
-##    print("Write successful")
+def Load_Settings():
+    global ADDRESS, MASK, PORT, log_file_name, log_separator
 
-try:
-    with open("MonZiot.json", "r") as jsonfile:
-        ConfData = json.load(jsonfile)
-        print("Read successful")
-except:
-    ConfData={}
+    print(" ========= Load_Settings() not yet working =================")
+    try:
+        with open("MonZiot.json", "r") as jsonfile:
+            ConfData = json.load(jsonfile)
+            print("Read configuration successful")
+    except:
+        ConfData={}
 
 
-Update_ConfData = False
+    Update_ConfData = False
 
-if "Ethernet" in ConfData:
-#-------------------------
-    print(" ** Ethernet y est")
-else:
-    ConfData["Ethernet"]={}
-    Update_ConfData = True
-    print("create Ethernet section ")
+    if "Ethernet" in ConfData:
+    #-------------------------
+        print(" ** Ethernet section")
+    else:
+        ConfData["Ethernet"]={}
+        Update_ConfData = True
+        print("create Ethernet section ")
 
-if "ADDRESS" in ConfData["Ethernet"]:
-    ADDRESS = ConfData["Ethernet"]["ADDRESS"]
-    print("ADDR = "+ADDRESS)
-else:
-    ADDRESS = "192.168.1.11"
-    ConfData["Ethernet"]["ADDRESS"]=ADDRESS
-    Update_ConfData = True
-    print("create ADDR = "+ADDRESS)
-if "MASK" in ConfData["Ethernet"]:
-    MASK = ConfData["Ethernet"]["MASK"]
-    print("MASK = "+MASK)
-else:
-    MASK = "255.255.0.0"
-    ConfData["Ethernet"]["MASK"]=MASK
-    Update_ConfData = True
-    print("create MASK = "+MASK)
-if "PORT" in ConfData["Ethernet"]:
-    PORT = int(ConfData["Ethernet"]["PORT"])
-    Update_ConfData = True
-    print("PORT = "+str(PORT))
-else:
-    PORT = 6789
-    ConfData["Ethernet"]["PORT"]=str(PORT)
-    print("create PORT = "+str(PORT))
+    if "ADDRESS" in ConfData["Ethernet"]:
+        ADDRESS = ConfData["Ethernet"]["ADDRESS"]
+        print("ADDR = "+ADDRESS)
+    else:
+        ADDRESS = "192.168.1.11"
+        ConfData["Ethernet"]["ADDRESS"]=ADDRESS
+        Update_ConfData = True
+        print("create ADDR = "+ADDRESS)
 
-if "Logger" in ConfData:
-#-------------------------
-    print(" ** Logger y est")
-else:
-    ConfData["Logger"]={}
-    Update_ConfData = True
-    print("create Logger section ")
+    if "MASK" in ConfData["Ethernet"]:
+        MASK = ConfData["Ethernet"]["MASK"]
+        print("MASK = "+MASK)
+    else:
+        MASK = "255.255.0.0"
+        ConfData["Ethernet"]["MASK"]=MASK
+        Update_ConfData = True
+        print("create MASK = "+MASK)
 
-if "LogFileName" in ConfData["Logger"]:
-    log_file_name = ConfData["Logger"]["LogFileName"]
-    print("LogFileName = "+log_file_name)
-else:
-    log_file_name = "MyLog.txt"
+    if "PORT" in ConfData["Ethernet"]:
+        PORT = int(ConfData["Ethernet"]["PORT"])
+        Update_ConfData = True
+        print("PORT = "+str(PORT))
+    else:
+        PORT = 6789
+        ConfData["Ethernet"]["PORT"]=str(PORT)
+        print("create PORT = "+str(PORT))
+
+    if "Logger" in ConfData:
+    #-------------------------
+        print(" ** Logger section")
+    else:
+        ConfData["Logger"]={}
+        Update_ConfData = True
+        print("create Logger section ")
+
+    if "LogFileName" in ConfData["Logger"]:
+        log_file_name = ConfData["Logger"]["LogFileName"]
+        print("LogFileName = "+log_file_name)
+    else:
+        log_file_name = "MyLog.txt"
+        ConfData["Logger"]["LogFileName"]=log_file_name
+        Update_ConfData = True
+        print("create LogFileName = "+log_file_name)
+
+    if "LogDelimiter" in ConfData["Logger"]:
+        log_separator = ConfData["Logger"]["LogDelimiter"]
+        print("LogDelimiter = "+log_separator)
+    else:
+        log_separator = ";"
+        ConfData["Logger"]["LogDelimiter"]=log_separator
+        Update_ConfData = True
+        print("create LogDelimiter = "+log_separator)
+
+
+    if Update_ConfData:
+        MyJSON = json.dumps(ConfData, indent=4, sort_keys=True)
+        with open("MonZiot.json", "w") as jsonfile:
+            jsonfile.write(MyJSON)
+            print("ReWrite configuration successful")
+
+
+
+
+
+def save_Settings():
+    global IP_StrVar, MASK_StrVar, Port_StrVar, log_file_name, log_separator
+    global ConfData
+
+    print(" ========= save_Settings() not yet working =================")
+
+    try:
+        with open("MonZiot.json", "r") as jsonfile:
+            ConfData = json.load(jsonfile)
+            print("Read successful")
+    except:
+        ConfData={}
+
+
+    if "Ethernet" in ConfData:
+    #-------------------------
+        print(" ** Ethernet y est")
+    else:
+        ConfData["Ethernet"]={}
+        print("create Ethernet section ")
+
+    ConfData["Ethernet"]["ADDRESS"] = IP_StrVar.get()
+    print("ADDR = "+IP_StrVar.get())
+
+    ConfData["Ethernet"]["MASK"]=MASK_StrVar.get()
+    print("MASK = "+MASK_StrVar.get())
+
+    ConfData["Ethernet"]["PORT"]=str(Port_StrVar.get())
+    print("PORT = "+str(Port_StrVar.get()))
+
+    if "Logger" in ConfData:
+    #-------------------------
+        print(" ** Logger y est")
+    else:
+        ConfData["Logger"]={}
+        print("Logger section ")
+
     ConfData["Logger"]["LogFileName"]=log_file_name
-    Update_ConfData = True
-    print("create LogFileName = "+log_file_name)
+    print("LogFileName = "+log_file_name)
 
-if "LogDelimiter" in ConfData["Logger"]:
-    log_separator = ConfData["Logger"]["LogDelimiter"]
-    print("LogDelimiter = "+log_separator)
-else:
-    log_separator = ";"
     ConfData["Logger"]["LogDelimiter"]=log_separator
-    Update_ConfData = True
-    print("create LogDelimiter = "+log_separator)
+    print("LogDelimiter = "+log_separator)
 
 
-if Update_ConfData:
     MyJSON = json.dumps(ConfData, indent=4, sort_keys=True)
     with open("MonZiot.json", "w") as jsonfile:
         jsonfile.write(MyJSON)
         print("ReWrite successful")
-
-
 
 
 
@@ -433,43 +474,76 @@ def changeConnStatus():
 
 
 
+# ---------- Popup Window for Rf Measurement -------------
+
+def Rf_create():
+    win = Toplevel(root)
+
+
+# ---------- Load setting at init -------------
+
+Load_Settings()
 
 # -------------- IP widget --------------------
 #ADDRESS = '192.168.1.52'
 #PORT = 6789
+NetworkTitle_label = Label(left_frame, text='Network',background = LABEL_BACKGROUND, fg = LABEL_FOREGROUND)
 IP_StrVar   = StringVar(left_frame, value=ADDRESS)
 IP_label    = Label(left_frame, text='IP address :',background = LABEL_BACKGROUND, fg = LABEL_FOREGROUND)
-IP_entry    = Entry(left_frame, textvariable = IP_StrVar, background=ENTRY_COLOR);
-MASK_StrVar = StringVar(left_frame, value='255.255.255.0')
+IP_entry    = Entry(left_frame, textvariable = IP_StrVar, width = ENTRY_DEF_WIDTH, background=ENTRY_COLOR);
+MASK_StrVar = StringVar(left_frame, value=MASK)
 MASK_label  = Label(left_frame, text='IP mask : ',background = LABEL_BACKGROUND, fg = LABEL_FOREGROUND)
-MASK_entry  = Entry(left_frame, textvariable = MASK_StrVar, background=ENTRY_COLOR)
+MASK_entry  = Entry(left_frame, textvariable = MASK_StrVar, width = ENTRY_DEF_WIDTH, background=ENTRY_COLOR)
 Port_StrVar = StringVar(left_frame, value=str(PORT))
 Port_label  =  Label(left_frame, text='IP port : ',background = LABEL_BACKGROUND, fg = LABEL_FOREGROUND)
-Port_entry  =  Entry(left_frame, textvariable = Port_StrVar, background=ENTRY_COLOR);
+Port_entry  =  Entry(left_frame, textvariable = Port_StrVar, width = ENTRY_DEF_WIDTH, background=ENTRY_COLOR);
 
+NetworkTitle_label.config(font='Helvetica 10 bold')
 ConnStatus_txt = StringVar()
 ConnStatus_label = Label(left_frame, textvariable=ConnStatus_txt,background = LABEL_BACKGROUND, fg = LABEL_FOREGROUND)
 ConnStatus_txt.set("----")
 ConnStatus_label.config(fg="black", font='Helvetica 10 bold')
 ConnStatus = 1
-Conn_bouton = Button(left_frame, text="Connect", command=changeConnStatus)
+Conn_bouton = Button(left_frame, text="Connect", width = BUTTON_DEF_WIDTH, command=changeConnStatus)
 Conn_bouton["fg"] = "black"
 
 
 ##entry_L = Entry(Right_frame, background="orange")
 
 # layout the widgets in the top frame
-Conn_bouton.grid(row=0, column=0)
-ConnStatus_label.grid(row=0, column=1)
-IP_label.grid(row=3, column=0)
-IP_entry.grid(row=3, column=1, padx=5)
-MASK_label.grid(row=4, column=0)
-MASK_entry.grid(row=4, column=1, padx=5)
-Port_label.grid(row=5, column=0)
-Port_entry.grid(row=5, column=1, padx=5)
+NetworkTitle_label.grid(row=0, column=0, columnspan =2, padx=10,pady=10)
+Conn_bouton.grid(row=1, column=0)
+ConnStatus_label.grid(row=1, column=1)
+IP_label.grid(row=4, column=0)
+IP_entry.grid(row=4, column=1, padx=5)
+MASK_label.grid(row=5, column=0)
+MASK_entry.grid(row=5, column=1, padx=5)
+Port_label.grid(row=6, column=0)
+Port_entry.grid(row=6, column=1, padx=5)
 
 
+# -------------- Config Widget --------------------
+ConfTitle_label    = Label(left_frame, text='Configuration',background = LABEL_BACKGROUND, fg = LABEL_FOREGROUND)
+Load_bouton        = Button(left_frame, text="Load JSON", width = BUTTON_DEF_WIDTH, command=Load_Settings)
+Load_bouton["fg"]  = "black"
+Save_bouton        = Button(left_frame, text="Save JSON", width = BUTTON_DEF_WIDTH, command=save_Settings)
+Load_bouton["fg"]  = "black"
 
+ConfTitle_label.config(font='Helvetica 10 bold')
+
+ConfTitle_label.grid(row=7, column=0, columnspan =2, pady=10)
+Load_bouton.grid(row=8, column=0, padx=10)
+Save_bouton.grid(row=8, column=1, padx=10)
+
+# -------------- Measure Rf --------------------
+MeasRfTitle_label    = Label(left_frame, text='Measure Rf',background = LABEL_BACKGROUND, fg = LABEL_FOREGROUND)
+MeasRf_bouton        = Button(left_frame, text="Start", width = BUTTON_DEF_WIDTH, command=Rf_create)
+MeasRf_bouton["fg"]  = "black"
+
+MeasRfTitle_label.config(font='Helvetica 10 bold')
+
+MeasRfTitle_label.grid(row=9, column=0, columnspan =2, pady=10)
+MeasRf_bouton.grid(row=10, column=0, padx=10)
 
 
 
